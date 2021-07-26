4c390678af07ee6fc1110ea7612be64e2bfd8056
Bekkelien <bekkelien@hotmail.com>
2021-07-26 10:47:40 +0200

    .

--- configuration.py
+++ configuration.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def read_config():
     fpath = Path().cwd() / "config.yaml"
-    print(fpath)
+
     with open(fpath) as f:
         config = yaml.load(f, yaml.Loader)
     return config
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     read_config()
     config = read_config()
     print(config["mail"])
-    
