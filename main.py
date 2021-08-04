0a239a726f9e590b10aac1121439403898d5d1eb
Espen Bekkelien <Bekkelien@hotmail.com>
2021-08-04 08:51:13 +0200

    fixed typo

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ while 1:
             sendmail()
             print("Alarm, mail i sendt out")
 
-            alar3m_activated = False
+            alarm_activated = False
 
         # If hasrate limit has been restored activate alarm again 
         if int(last_reported_hashrate) >= int(hashrate_limit):
